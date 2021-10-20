c6b30bd8fccceac1a91a41716b8b83c8eb0bcbe8
Dan Zhao <dan.zhao@mygeorgian.ca>
2021-10-20 12:01:12 -0400

    third

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./controllers/index');
+var usersRouter = require('./controllers/users');
+var teetimeRouter = require('./controllers/teetime');
+
 
 var app = express();
 
@@ -21,6 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/teetime',teetimeRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
